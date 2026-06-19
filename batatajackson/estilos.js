91ae1ed9403926721d721ffb407ca899cba88acf
KyaraFerreiraGonc <kyaraetecgoncalves@gmail.com>
2026-06-19 00:08:59 +0000

    final Aula_07

--- batatajackson/estilos.js
+++ batatajackson/estilos.js
@@ -3,42 +3,48 @@ import { StyleSheet } from "react-native";
 const estilos = StyleSheet.create({
   container: {
     width: "100%",
-    height: "100%",
-    margin: "auto",
+    minHeight: "100%",
+    padding: 10,
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: "#dadadaff",
-
-   
+    backgroundColor: "rgb(177, 172, 172)",
   },
   texto: {
-    // fontSize: 50,
-    // marginBottom: 20,
-    // marginTop: 30,
-    // fontStyle: "Bold",
-  },
-
-  BoxIcones: {
-
-
-  },
-
-  boxTitulo:{
-    fontSize: 40,
+    fontSize: 50,
     marginBottom: 20,
     marginTop: 30,
     fontStyle: "Bold",
-    backgroundColor: "#ffffffff",
+  },
+
+  BoxIcones: {
+    // width: "100%",
+    // alignItems: "center",
+  },
+
+  boxTitulo:{
+    fontSize: 20,
+    marginTop: 15,
+    marginBottom: 0,
+    fontStyle: "Bold",
+    backgroundColor: "white",
+    width: 350,
+    padding: 10,
+    textAlign: "left",
+    borderRadius: 5,
   },
 
   IconesLista:{
-  display: "flex",
+    display: "flex",
     flexDirection: "row",
-    width: "100%",
-    height: "10%",
-    // justifyContent: "center",
-    // alignItems: "center",
-    backgroundColor: "#ffffffff",
+    width: 350,
+    flexWrap: "wrap",
+    justifyContent: "space-around",
+    alignItems: "center",
+    backgroundColor: "white",
+    padding: 10,
+    borderRadius: 5,
+    marginBottom: 15,
+    marginTop: -10,
   }
 
 });
